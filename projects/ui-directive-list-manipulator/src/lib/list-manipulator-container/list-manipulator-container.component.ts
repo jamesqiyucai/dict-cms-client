03ac8be7805dc94993cf5c241902eb133e8ad4bd
jihuzzi <jamestsai@James-Thinkpad.localdomain>
2020-10-27 21:50:04 +0800

    move forward with list manipulator

--- projects/ui-directive-list-manipulator/src/lib/list-manipulator-container/list-manipulator-container.component.ts
+++ projects/ui-directive-list-manipulator/src/lib/list-manipulator-container/list-manipulator-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, TemplateRef, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-list-manipulator-container',
@@ -7,6 +7,19 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListManipulatorContainerComponent implements OnInit {
+
+  private childTemplate: TemplateRef<any>;
+
+  private elementModels: any[];
+
+  @Input() public set data(newData: TemplateRef<any>) {
+    this.childTemplate = newData;
+    
+  }
+
+  @Input() public set model(elementModels: any[]) {
+    this.elementModels = elementModels;
+  }
 
   constructor() { }
 
--- projects/ui-directive-list-manipulator/src/lib/list-manipulator.directive.ts
+++ projects/ui-directive-list-manipulator/src/lib/list-manipulator.directive.ts
@@ -1,13 +1,29 @@
-import { Directive, ViewContainerRef, TemplateRef } from '@angular/core';
+import { createComponent } from '@angular/compiler/src/core';
+import { ComponentFactoryResolver, Directive, TemplateRef, ViewContainerRef, OnInit, Input } from '@angular/core';
+import { ListManipulatorContainerComponent } from './list-manipulator-container/list-manipulator-container.component';
 
 @Directive({
   selector: '[libListManipulator]'
 })
-export class ListManipulatorDirective {
+export class ListManipulatorDirective implements OnInit {
 
   constructor(
     private viewContainerRef: ViewContainerRef,
-    private templateRef: TemplateRef<any>
+    private templateRef: TemplateRef<any>,
+    private componentFactoryResolver: ComponentFactoryResolver
   ) { }
+
+  ngOnInit() {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ListManipulatorContainerComponent);
+    const containerComponentRef = this.viewContainerRef.createComponent<ListManipulatorContainerComponent>(componentFactory);
+    containerComponentRef.instance.data = this.templateRef;
+  }
+
+  @Input() public set models(elementModels: any[]) {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ListManipulatorContainerComponent);
+    const containerComponentRef = this.viewContainerRef.createComponent<ListManipulatorContainerComponent>(componentFactory);
+    containerComponentRef.instance.data = this.templateRef;
+    containerComponentRef.instance.model = elementModels;
+  }
 
 }
