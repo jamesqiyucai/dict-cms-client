a500efa70d0f9ccea01381717fda3a1e3d676539
jamesqiyucai <jamestsai@James-Surface-Go.localdomain>
2020-10-30 08:21:33 +0800

    updated compile file

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ ng build service-source --prod
 ng build ui-model-editable --prod
 ng build ui-model-source --prod
 ng build ui-component-source --prod
+ng build ui-directive-editable --prod
+ng build ui-directive-list-manipulator --prod
