78d57099898cc037887e6fa5f2dbc00581d84f93
jihuzzi <jamestsai@James-Thinkpad.localdomain>
2020-11-27 23:18:35 +0800

    switch to ubuntu

--- projects/ui-model-example-source-book/src/lib/example-source-book-model-factory.ts
+++ projects/ui-model-example-source-book/src/lib/example-source-book-model-factory.ts
@@ -4,13 +4,13 @@ import {SourceHandle, SourceType} from 'service-source';
 import {ExampleSourceBookHandle} from 'service-example-source-book';
 import {ExampleSourceBookModel} from './example-source-book-model';
 
-function isViewableBookSourceHandle(handle: unknown): handle is ExampleSourceBookHandle {
+function isExampleSourceBookHandle(handle: unknown): handle is ExampleSourceBookHandle {
   return handle && (handle as SourceHandle).type == SourceType.Book && (handle as SourceHandle).editable == false;
 }
 
 export class ViewableBookSourceModelFactory implements SourceFactory {
   createSource(handle: unknown): ExampleSourceBookModel | null {
-    if (isViewableBookSourceHandle(handle)) {
+    if (isExampleSourceBookHandle(handle)) {
       return new ExampleSourceBookModelImpl(handle);
     } else {
       return null;
